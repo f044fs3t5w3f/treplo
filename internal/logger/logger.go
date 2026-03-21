59c77bc9b7812fbdcfe65c2d15c16c2e32b897d2
Anton Kuleshov <akyla10@yandex.ru>
2026-03-22 02:24:54 +0300

    logger

---
+++ internal/logger/logger.go
@@ -0,0 +1,25 @@
+package logger
+
+import (
+	"context"
+	"log/slog"
+)
+
+type loggerKeyType struct{}
+
+var loggerKey = loggerKeyType{}
+
+func GetLogger() *slog.Logger {
+	logger := slog.Default()
+	return logger
+}
+
+func WithLogger(ctx context.Context, logger *slog.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey, logger)
+}
+func FromContext(ctx context.Context) *slog.Logger {
+	if logger, ok := ctx.Value(loggerKey).(*slog.Logger); ok {
+		return logger
+	}
+	return slog.Default() // fallback
+}
