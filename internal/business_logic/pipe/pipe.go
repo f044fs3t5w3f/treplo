be5fd1a55cdbb80ef6fce22450549c633204cfb2
Anton Kuleshov <akyla10@yandex.ru>
2026-03-24 00:40:38 +0300

    ...

--- internal/business_logic/pipe/pipe.go
+++ internal/business_logic/pipe/pipe.go
@@ -10,53 +10,64 @@ import (
 	"github.com/a-kuleshov/treplo/internal/business_logic/pipe/tasker"
 	"github.com/a-kuleshov/treplo/internal/business_logic/pipe/uploader"
 	"github.com/a-kuleshov/treplo/internal/business_logic/pipe/waiter"
+	"github.com/a-kuleshov/treplo/internal/logger"
 	"github.com/a-kuleshov/treplo/internal/models"
 	"github.com/a-kuleshov/treplo/pkg/sber/salute"
 	tgBotApi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+const bufSize = 10
+
 type pipe struct {
-	processors []FileProcessor
-	repo       repository
+	// processors []FileProcessor
+	// repo       repository
+	ch chan *models.File
 }
 
-type tgNotifier struct {
-	tgbotapi *tgBotApi.BotAPI
-}
-
-func (tn tgNotifier) Notify(replyToMessageId int, chatId int64, message string) error {
-	tgMessage := tgBotApi.NewMessage(chatId, message)
-	tgMessage.ReplyToMessageID = replyToMessageId
-	_, err := tn.tgbotapi.Send(tgMessage)
-	return err
-}
-
-func NewPipe(repo repository, tgbotapi *tgBotApi.BotAPI, saluteApi *salute.SpeachService, storagePath string) (pipe, error) {
+func NewPipe(ctx context.Context, repo repository, tgbotapi *tgBotApi.BotAPI, saluteApi *salute.SpeachService, storagePath string) (pipe, error) {
 	downloaderProcessor, err := downloader.NewDownloader(tgbotapi.GetFileDirectURL, storagePath)
 	if err != nil {
 		return pipe{}, fmt.Errorf("downloader.NewDownloader: %w", err)
 	}
+	processors := []FileProcessor{
+		downloaderProcessor,
+		&uploader.FileUploader{Uploader: saluteApi},
+		&tasker.Tasker{Tasker: saluteApi},
+		&waiter.Waiter{StatusChecker: saluteApi},
+		&content_downloader.Tasker{Downloader: saluteApi},
+		&notifier.NotifyProccessor{Notifier: tgNotifier{tgbotapi: tgbotapi}},
+	}
+	channels := make([]chan *models.File, len(processors))
+
+	for i := range channels {
+		channels[i] = make(chan *models.File, bufSize)
+	}
+	pipeInputChannel := channels[0]
+	for i, processor := range processors {
+		inputChannel := channels[i]
+		var outputChannel chan *models.File
+		if i < len(channels)-1 {
+			outputChannel = channels[i+1]
+		}
+		go func() {
+			for file := range inputChannel {
+				processor.Process(ctx, file)
+				if err := repo.SaveFile(ctx, file); err != nil {
+					logger.FromContext(ctx).Error("pipe: repo.SaveFile: %w", err)
+					continue
+				}
+				if outputChannel != nil {
+					outputChannel <- file
+				}
+			}
+		}()
+	}
+
 	return pipe{
-		processors: []FileProcessor{
-			downloaderProcessor,
-			&uploader.FileUploader{Uploader: saluteApi},
-			&tasker.Tasker{Tasker: saluteApi},
-			&waiter.Waiter{StatusChecker: saluteApi},
-			&content_downloader.Tasker{Downloader: saluteApi},
-			&notifier.NotifyProccessor{Notifier: tgNotifier{tgbotapi: tgbotapi}},
-		},
-		repo: repo,
+		ch: pipeInputChannel,
 	}, nil
 }
 
 func (p pipe) Process(ctx context.Context, file *models.File) error {
-	for _, processor := range p.processors {
-		if err := processor.Process(ctx, file); err != nil {
-			return err
-		}
-		if err := p.repo.SaveFile(ctx, file); err != nil {
-			return fmt.Errorf("repo.SaveFile: %w", err)
-		}
-	}
-	return nil
+	p.ch <- file
 }
